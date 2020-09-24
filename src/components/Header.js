cda6cd6d471369f092e193613fe3ffef6bba276c
Durgesh2398 <2308durgesh@gmail.com>
2020-09-24 23:02:18 +0530

    assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 // This Component is for Header which contains my logo
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <img src={logo} alt="website logo" style={{ height: "50%" }} />
+        <img src="https://visme.co/blog/wp-content/uploads/2017/08/40-Creative-Logo-Designs-to-Inspire-You-Databerry-2.jpg" alt="website logo" style={{ height: "50%" }} />
       </nav>
     </header>
   );
